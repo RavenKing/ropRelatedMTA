88f7f558fa0e81db9ba56062125f15c61544e321
Yang <kevin.yang02@sap.com>
2021-07-14 16:14:51 +0800

    111

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,8 @@ import Header from "./components/Header";
 import JsonData from "./data/skill.json";
 import "@ui5/webcomponents-fiori/dist/ShellBar.js";
 import ForceGraph from "./components/ForceGraph";
-import { Tab,TabContainer } from '@ui5/webcomponents-react';
+import TreeGraph from "./components/TreeGraph";
+import { Tab, TabContainer } from "@ui5/webcomponents-react";
 import "@ui5/webcomponents-icons/dist/Assets.js";
 
 // import data from './data/layer3.json'
@@ -18,56 +19,70 @@ function App() {
 
   return (
     <div className="container">
-      <ui5-shellbar
-        primary-title="Skill Cockpit"
-       
-      >
+      <ui5-shellbar primary-title="Skill Cockpit">
         <ui5-avatar slot="profile" icon="customer"></ui5-avatar>
-        <img slot="logo" src="https://sap.github.io/ui5-webcomponents/assets/images/sap-logo-svg.svg" />
+        <img
+          slot="logo"
+          src="https://sap.github.io/ui5-webcomponents/assets/images/sap-logo-svg.svg"
+        />
         <ui5-button icon="nav-back" slot="startButton"></ui5-button>
       </ui5-shellbar>
 
       <TabContainer
-  className=""
-  icon="sys-monitor"
-  onTabSelect={function noRefCheck(){}}
-  selected
-  semanticColor="Positive"
-  slot=""
-  style={{}}
-  text="Skill Relationship"
-  tooltip=""
->
-  <Tab
-    className=""
-    icon="sys-monitor"
-    onTabSelect={function noRefCheck(){}}
-    selected
-    semanticColor="Positive"
-    slot=""
-    style={{}}
-    tabLayout="Standard"
-    tabsPlacement="Top"
-    text="Skill Relationship"
-    tooltip=""
-  >
-      <Header
-      skills={skills}
-        searchSkill={searchSkill}
-        skill={skill}
-      />
-     
-      {/* <React.Fragment>
+        className=""
+        icon="sys-monitor"
+        onTabSelect={function noRefCheck() {}}
+        selected
+        semanticColor="Positive"
+        slot=""
+        style={{}}
+        text="Skill Relationship"
+        tooltip=""
+      >
+        <Tab
+          className=""
+          icon="sys-monitor"
+          onTabSelect={function noRefCheck() {}}
+          semanticColor="Positive"
+          selected
+          slot=""
+          style={{}}
+          tabLayout="Standard"
+          tabsPlacement="Top"
+          text="Skill Relationship"
+          tooltip=""
+        >
+          <Header skills={skills} searchSkill={searchSkill} skill={skill} />
+
+          {/* <React.Fragment>
         <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
       </React.Fragment> */}
 
-      <React.Fragment>
-        <ForceGraph skill={skill} />
-      </React.Fragment>
-  </Tab>
-</TabContainer>
+          <React.Fragment>
+            <ForceGraph skill={skill} />
+          </React.Fragment>
+        </Tab>
+        <Tab
+          className=""
+          icon="sys-monitor"
+          onTabSelect={function noRefCheck() {}}
+          semanticColor="Positive"
+          slot=""
+          style={{}}
+          tabLayout="Standard"
+          tabsPlacement="Top"
+          text="Skill Tree"
+          tooltip=""
+        >
+          <React.Fragment>
+            <TreeGraph></TreeGraph>
+          </React.Fragment>
 
-    
+          {/* <React.Fragment>
+        <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
+      </React.Fragment> */}
+        </Tab>
+      </TabContainer>
     </div>
   );
 }
